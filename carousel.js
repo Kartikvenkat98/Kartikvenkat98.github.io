8d1b00dd8192176452d826c84e98d89b4d443f25
kartik27 <kartikvenkat98@gmail.com>
2016-09-09 01:10:30 +0530

    Add responsiveness to the carousel

--- carousel.js
+++ carousel.js
@@ -1,16 +1,28 @@
 var m = 1;
 
+function plusSlides(n) {
+  m = m + n;
+  showSlides();
+}
+
 function currentSlide(n) {
 	m = n;
   	showSlides();
   	halt();
 }
 
+function currentSlides(n) {
+  m = n;
+  showSlides();
+}
+
+
 function showSlides(){
   	var i;
   	var slides = document.getElementsByClassName("mySlides");
   	var labels = document.getElementsByClassName("label");
-	
+	  var dots = document.getElementsByClassName("dot");
+
   	if (m > slides.length) {
   		m = 1;
   	}
@@ -20,8 +32,12 @@ function showSlides(){
   	for (i = 0; i < labels.length; i++) {
 	  	labels[i].className = labels[i].className.replace(" active", "");
   	}
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
   	slides[m - 1].style.display = "block";  
   	labels[m - 1].className += " active";
+    dots[m-1].className += " active";
   	m = m + 1; 
 }
 
